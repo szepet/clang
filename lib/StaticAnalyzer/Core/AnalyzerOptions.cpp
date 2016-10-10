a89a0c05644c0a2bfd3d0a32a240647985c25cc1
Gabor Horvath <gabor.a.horvath@ericsson.com>
2016-10-10 17:00:17 +0200

    Option to not to use visitedFunc.txt. Fixes #3.

--- lib/StaticAnalyzer/Core/AnalyzerOptions.cpp
+++ lib/StaticAnalyzer/Core/AnalyzerOptions.cpp
@@ -357,3 +357,10 @@ StringRef AnalyzerOptions::getXTUDir() {
     XTUDir = getOptionAsString("xtu-dir", "");
   return XTUDir.getValue();
 }
+
+bool AnalyzerOptions::shouldReanalyzeXTUVisitedFns() {
+  if (!ReanalyzeXTUVisitedFns.hasValue())
+    ReanalyzeXTUVisitedFns =
+        getBooleanOption("reanalyze-xtu-visited", /*Default=*/false);
+  return ReanalyzeXTUVisitedFns.getValue();
+}
